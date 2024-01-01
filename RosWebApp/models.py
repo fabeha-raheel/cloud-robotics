b2e8f3e7c28aeabc277ace5563a5d3fe54571744
fabeha-raheel <fabeha.raheel.aeelab@gmail.com>
2024-01-01 15:53:45 +0300

    created new field in django models called robot_slug

--- RosWebApp/models.py
+++ RosWebApp/models.py
@@ -5,7 +5,8 @@ from django.db import models
 # table name is RoswebApp_robot
 class Robot(models.Model):
     robot_name = models.CharField(max_length=30)
-    robot_type = models.CharField(max_length=30) 
+    robot_type = models.CharField(max_length=30)
+    robot_slug = models.SlugField() 
     robot_icon = models.ImageField(upload_to='RosWebApp/images', default='')
 
     def __str__(self):
