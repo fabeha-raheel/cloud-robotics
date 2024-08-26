a25dd45853c8d19dfdfb59a9f010717e2e9b7095
fabeha-raheel <fabeha.raheel.aeelab@gmail.com>
2024-08-26 11:29:18 +0300

    modified app views, configured urls.py

--- cloud_robotics_app/views.py
+++ cloud_robotics_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Robot
 
 # Create your views here.
+
+def home(request):
+    robots = Robot.objects.all()
+    context = {'robots': robots}
+    return render(request, 'cloud_robotics_app/frontpg.html', context)
+
+def robot_page(request, slug):
+    robot = Robot.objects.get(robot_slug=slug)
+    context = {'robot': robot}
+    return render(request, 'cloud_robotics_app/robotpg.html', context)
